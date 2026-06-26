914e4e4d2420c44e2f5ba5f6db2e0cd77e829aa0
icadorin <cadorinisrael@gmail.com>
2026-06-26 01:05:26 -0300

    feat: add conditional mapping for navy, bioimpedance and skinfold in mapper

--- src/main/java/com/bioimpedance/mapper/AssessmentMapper.java
+++ src/main/java/com/bioimpedance/mapper/AssessmentMapper.java
@@ -1,9 +1,9 @@
 package com.bioimpedance.mapper;
 
+import com.bioimpedance.constants.AssessmentMethod;
 import com.bioimpedance.dto.request.AssessmentRequestDTO;
 import com.bioimpedance.dto.request.CalculateRequestDTO;
-import com.bioimpedance.dto.response.AssessmentResponseDTO;
-import com.bioimpedance.dto.response.CalculationResultDTO;
+import com.bioimpedance.dto.response.*;
 import com.bioimpedance.entity.Assessment;
 import com.bioimpedance.entity.AssessmentResult;
 import org.mapstruct.Mapper;
@@ -16,14 +16,17 @@ import org.mapstruct.ReportingPolicy;
 )
 public interface AssessmentMapper {
 
+    @Mapping(target = "navy", expression = "java(mapToNavy(assessment))")
+    @Mapping(target = "bioimpedance", expression = "java(mapToBioimpedance(assessment))")
+    @Mapping(target = "skinfold", expression = "java(mapToSkinfold(assessment))")
+    AssessmentResponseDTO toResponse(Assessment assessment);
+
     @Mapping(target = "result", ignore = true)
     @Mapping(target = "id", ignore = true)
     @Mapping(target = "userId", ignore = true)
     @Mapping(target = "createdAt", ignore = true)
     @Mapping(target = "updatedAt", ignore = true)
     Assessment toEntity(AssessmentRequestDTO dto);
-
-    AssessmentResponseDTO toResponse(Assessment assessment);
 
     @Mapping(target = "methodDetails", ignore = true)
     @Mapping(target = "protein", ignore = true)
@@ -42,4 +45,36 @@ public interface AssessmentMapper {
     @Mapping(target = "createdAt", ignore = true)
     @Mapping(target = "updatedAt", ignore = true)
     Assessment toEntityFromCalculate(CalculateRequestDTO dto);
+
+    default NavyDataDTO mapToNavy(Assessment a) {
+        if (a.getMethod() != AssessmentMethod.NAVY) return null;
+        return NavyDataDTO.builder()
+            .waist(a.getWaist())
+            .neck(a.getNeck())
+            .hip(a.getHip())
+            .build();
+    }
+
+    default BioimpedanceDataDTO mapToBioimpedance(Assessment a) {
+        if (a.getMethod() != AssessmentMethod.BIOIMPEDANCE) return null;
+        return BioimpedanceDataDTO.builder()
+            .resistance(a.getResistance())
+            .reactance(a.getReactance())
+            .build();
+    }
+
+    default SkinfoldDataDTO mapToSkinfold(Assessment a) {
+        if (a.getMethod() != AssessmentMethod.SKINFOLD) return null;
+        return SkinfoldDataDTO.builder()
+            .protocol(a.getProtocol())
+            .biceps(a.getBiceps())
+            .chest(a.getChest())
+            .midaxillary(a.getMidaxillary())
+            .triceps(a.getTriceps())
+            .subscapular(a.getSubscapular())
+            .abdominal(a.getAbdominal())
+            .suprailiac(a.getSuprailiac())
+            .thigh(a.getThigh())
+            .build();
+    }
 }
