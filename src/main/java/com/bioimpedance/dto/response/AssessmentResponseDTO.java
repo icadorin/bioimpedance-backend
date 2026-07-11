4203098e84f7cb2aad367021ecb9d0d0eb58b719
icadorin <cadorinisrael@gmail.com>
2026-07-10 23:56:29 -0300

    feat(assessment): add clientName field to AssessmentResponseDTO

--- src/main/java/com/bioimpedance/dto/response/AssessmentResponseDTO.java
+++ src/main/java/com/bioimpedance/dto/response/AssessmentResponseDTO.java
@@ -14,6 +14,7 @@ import java.time.LocalDateTime;
 public class AssessmentResponseDTO {
     private String id;
     private String clientId;
+    private String clientName;
     private LocalDate date;
     private AssessmentMethod method;
 
